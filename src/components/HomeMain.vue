2b9db064043b57b1174450b68d55d9d4a382e784
吕海东 <haidonglu@126.com>
2020-07-15 16:52:41 +0800

    第2次提交

---
+++ src/components/HomeMain.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>系统主页</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export  default class HomeMain extends Vue{
+	@Prop()
+	private title!: string;
+}
+	
+</script>
+
+<style>
+</style>
